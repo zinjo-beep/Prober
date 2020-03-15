bd7ce1dc26f71f57477b07e45355edd47f08c917
Felix <QuadeFe@whs.school.nz>
2020-03-16 12:15:53 +1300

    Initial sharing of project

---
+++ Prober.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Prober here.
+ *
+ * @author (felx)
+ * @version (the best version)
+ */
+public class Prober
+{
+    // instance variables - replace the example below with your own
+    //private int x;
+    public Prober()
+    {
+        // initialise instance variables
+        Scanner inputStream = new Scanner (System.in);
+        int playerInput = 0;
+        int compRespsonse = 0;
+        System.out.println("Cooperate or defect?");
+        
+
+    }
+}
