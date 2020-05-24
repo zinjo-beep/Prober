5d07a51c4186e776e856995a22b48ddbc8b9fa8d
Felix <QuadeFe@whs.school.nz>
2020-05-25 11:47:34 +1200

    This is now the Pre-Alpha shell for the rest of my project -Removed previous prober code -Added "while" so the program continues until told to finish

--- Prober.java
+++ Prober.java
@@ -2,21 +2,35 @@ import java.util.Scanner;
 /**
  * Write a description of class Prober here.
  *
- * @author (felx)
- * @version (the best version)
+ * @author (Felix)
+ * @version (Pre-Alpha)
  */
 public class Prober
 {
-    // instance variables - replace the example below with your own
-    //private int x;
+    private int i;
     public Prober()
     {
         // initialise instance variables
         Scanner inputStream = new Scanner (System.in);
-        int playerInput = 0;
-        int compRespsonse = 0;
-        System.out.println("Cooperate or defect?");
-        
-
+        String playerInput;
+        String compResponse;
+        int round = 1;
+        boolean finish = false;
+        while (!finish){
+            System.out.println("--------------------");
+            System.out.println("Cooperate or defect?");
+            playerInput=inputStream.nextLine();
+            switch (playerInput) {
+                //the prints are just to check the t4t status
+                case "c" : System.out.println("You cooperated");
+                            break;
+                case "d" : System.out.println("You defected");
+                            break;
+                default : System.out.println("Invalid input");
+                            break;
+                 } 
+            compResponse = "d";
+            round += 1;
+            }
     }
 }
